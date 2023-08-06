75c9d1a67779b0dc543cbfb1a3496e14db552c4c
Leandro <leandro_ggg@hotmail.com>
2023-08-06 20:42:53 -0300

    first commit

---
+++ src/components/book.js
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom";
+
+
+export default  function Book({item}){
+    const booksContainerStyle= {
+        display: 'flex',
+        flexDirection:'column',
+        with:"300px",
+    };
+    const bookInfoStyle= {
+        display: "flex",
+        flexDirection:"column",
+        alignItems:"center",
+        textAlign:"center",
+        color:"white",
+        textDecoration:"none",
+    };
+    
+    return(
+    <div style={booksContainerStyle}>
+        <Link to={`/view/${item.id}`} style={bookInfoStyle}>
+        <img src={item.cover} width="200" alt={item.title}/>
+        <div>{item.title}</div>
+        </Link>
+    </div>
+    );
+
+}
